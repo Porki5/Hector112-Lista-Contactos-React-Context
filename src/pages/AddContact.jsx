2ee3a74ec2c956e0a3410939ab286eb6d145a322
Héctor Fernández <hectorferca@gmail.com>
2025-08-11 07:37:13 +0000

    f

--- src/pages/AddContact.jsx
+++ src/pages/AddContact.jsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { AContactoComp} from "../components/AContactoComp.jsx";
 
 export const AddContact = () => {
 
     let user = localStorage.getItem("usuario"); //traemos el nombre del usuario desde localStorage
+    const { id } = useParams(); // Para saber si es edición o creación
 
     return (
         <div className="container w-75 text-center mt-5">
-            <h1>Añade un contacto a la agenda de {user}</h1>            
+            <h1>{id ? "Editar" : "Crear"} nuevo contacto en la agenda de {user}</h1>            
 
             <AContactoComp />
             <Link to="/" onClick={() => localStorage.setItem("fromForm", "true")}>Volver a inicio</Link>
